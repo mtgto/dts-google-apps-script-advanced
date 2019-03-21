bdbe7b8628fe503e9d925cb690660eadd0f1099e
mtgto <hogerappa@gmail.com>
2019-03-21 23:01:50 +0900

    print hierarchy

---
+++ README.md
@@ -0,0 +1,32 @@
+dts-google-apps-script-advanced
+====
+
+d.ts files generator for [Advanced Services of Google Apps Script](https://developers.google.com/apps-script/guides/services/advanced).
+
+Inspire from [motemen/dts-google-apps-script](https://github.com/motemen/dts-google-apps-script).
+
+# How To Generate
+
+Require `node.js` and `TypeScript`.
+
+You need to download 
+
+```console
+$ tsc index.ts
+$ node index.js gas-advanced-service-definition-for-autocomplete.json
+```
+
+# How To Download `gas-advanced-service-definition-for-autocomplete.json`
+
+1. Open editor of Google Apps Script.
+1. Enable Advanced Service in menu.
+1. Browser download it.
+1. Trim first line (it is broken JSON!).
+
+# Support
+
+- [Admin Directory API](https://developers.google.com/admin-sdk/directory/v1/reference/)
+
+# License
+
+MIT
--- index.ts
+++ index.ts
@@ -1,49 +1,130 @@
 import * as fs from "fs";
 
-const dict: object = JSON.parse(fs.readFileSync('./admin_directory.json', 'utf8'));
-// Object.keys(dict).forEach(key => {
-//   const child: object = dict[key];
-//   Object.keys(child).forEach(key => {
-//     const grandchild: object = child[key];
-//     if (typeof grandchild === "string") {
-//       console.log("TARGET: " + grandchild);
-//       return;
-//     }
-//     Object.keys(grandchild).forEach(key => {
-//       console.log(grandchild[key]);
-//     });
-//   });
-// });
+class Namespace {
+  /// Full namespace name. ex. "Admin_directory_v1.Admin.Directory_v1.Collection.AspsCollection"
+  names: string[];
+  /// last name of fullname. ex. "AspsCollection"
+  name: string;
+  children: Namespace[];
+  interfaces: Interface[];
 
-const printObj = (obj: object): void => {
+  constructor(fullname: string) {
+    this.names = fullname.split(".");
+    this.name = nameFromFullname(fullname);
+    this.children = [];
+    this.interfaces = [];
+  }
+
+  add(i: Interface): void {
+    if (this.names.length + 1 === i.names.length) {
+      this.interfaces.push(i);
+    } else if (this.names.length + 1 < i.names.length) {
+      const childName = i.names[this.names.length];
+      let child = this.children.find(child => child.name === childName);
+      if (!child) {
+        child = new Namespace(this.names.join(".") + "." + childName);
+        this.children.push(child);
+      }
+      child.add(i);
+    }
+  }
+
+  print(depth: number): void {
+    console.log(`${depth === 0 ? "declare " : ""}${" ".repeat(depth * 2)}namespace ${this.name} {`);
+    this.children.forEach(child => {
+      child.print(depth+1);
+    })
+    this.interfaces.forEach(i => {
+      i.print(depth+1);
+    })
+    console.log(`${" ".repeat(depth * 2)}}`);
+  }
+}
+
+class Interface {
+  /// Full namespace name. ex. "Admin_directory_v1.Admin.Directory_v1.Collection.AspsCollection"
+  names: string[];
+  /// last name of fullname. ex. "AspsCollection"
+  name: string;
+  fields: object[];
+  methods: object[];
+
+  constructor(fullname: string, fields: object[], methods: object[]) {
+    this.names = fullname.split(".");
+    this.name = nameFromFullname(fullname);
+    this.fields = fields;
+    this.methods = methods;
+  }
+
+  print(depth: number): void {
+    const indent = " ".repeat(depth * 2);
+    console.log(`${indent}export interface ${this.name} {`);
+    this.fields.forEach(field => {
+      let type: string = this.normalizeTypeName(field["2"]);
+      console.log(`  ${indent}${field["1"]}: ${type};`)
+    })
+    this.methods.forEach(method => {
+      method["6"].split("\n").forEach(comment => {
+        if (comment.length > 0) {
+          console.log(`  ${indent}// ${comment}`);
+        }
+      });
+      let line = `  ${indent}${method["1"]}(`;
+      if (Array.isArray(method["3"])) {
+        // has arguments
+        const args: string[] = method["3"].map(arg => {
+          let type: string = this.normalizeTypeName(arg["2"]);
+          return `${arg["1"]}: ${type}`;
+        });
+        line = line + args.join(", ");
+      }
+      line = line + `): ${method["2"]};`
+      console.log(line);
+    })
+    console.log(`${indent}}`);
+  }
+
+  normalizeTypeName(type: string): string {
+    if (type === "Integer") {
+      return "number";
+    } else if (type === "Integer[]") {
+      return "number[]";
+    } else if (type === "Byte[]") {
+      return "string";
+    } else if (type === "String") {
+      return "string";
+    } else if (type === "Object") {
+      return "object";
+    }
+    return type;
+  }
+}
+
+const convert = (obj: object): Interface => {
   const name = obj["1"];
   const fields: object[] = obj["2"] || [];
   const methods: object[] = obj["3"] || [];
-  console.log(`export interface ${name} {`);
-  fields.forEach(field => {
-    console.log(`  ${field["1"]}: ${field["2"]};`)
-  })
-  methods.forEach(method => {
-    console.log(`  // ${method["6"]}`);
-    let line = `  ${method["1"]}(`;
-    if (Array.isArray(method["3"])) {
-      // has arguments
-      const args: string[] = method["3"].map(arg => {
-        return `${arg["1"]}: ${arg["2"]}`
-      });
-      line = line + args.join(", ");
-    }
-    line = line + `): ${method["2"]};`
-    console.log(line);
-  })
-  console.log(`}`);
-};
+  return new Interface("GoogleAppsScript." + name, fields, methods);
+}
 
+const nameFromFullname = (fullname: string): string => {
+  const names: string[] = fullname.split(".");
+  return names[names.length - 1];
+}
+
+if (process.argv.length < 3) {
+  console.log("Usage: node index.js <json>");
+  process.exit(1);
+}
+
+const dict: object = JSON.parse(fs.readFileSync(process.argv[2], 'utf8'));
+const root = new Namespace("GoogleAppsScript");
 
 // Main interface
 let obj = dict["1"];
-printObj(obj);
+root.add(convert(obj));
 
 dict["2"].forEach(cls => {
-  printObj(cls);
+  root.add(convert(cls));
 });
+root.print(0);
